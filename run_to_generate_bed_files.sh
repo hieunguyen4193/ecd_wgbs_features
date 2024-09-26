2f317ab7546c9aa366fa422a7340088df7a8725d
hieunguyen4193 <work@hieunguyen.de>
2024-09-26 09:23:56 +0200

    update

---
+++ run_to_generate_bed_files.sh
@@ -0,0 +1,4 @@
+python generate_CpG_clusters.py --input /media/hieunguyen/HNSD01/resources/hg19 --output ./hg19_CpG_clusters --radius 100 --min_num_cpg 5
+python generate_CpG_clusters.py --input /media/hieunguyen/HNSD01/resources/hg19 --output ./hg19_CpG_clusters --radius 200 --min_num_cpg 5
+python generate_CpG_clusters.py --input /media/hieunguyen/HNSD01/resources/hg19 --output ./hg19_CpG_clusters --radius 200 --min_num_cpg 10
+python generate_CpG_clusters.py --input /media/hieunguyen/HNSD01/resources/hg19 --output ./hg19_CpG_clusters --radius 300 --min_num_cpg 10
